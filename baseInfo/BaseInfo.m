98371d4cbfca055cfef6a36f353a7ce79cd1994c
@单小飞 <shanpengfei@zhidaochuxing.com>
2017-02-16 15:09:00 +0800

    真正书写的时候遇到了问题   在不使用第三方的情况下怎么才能够简便的通过字典或者json串创建生成一个类

--- baseInfo/BaseInfo.m
+++ baseInfo/BaseInfo.m
@@ -9,8 +9,19 @@
 #import "BaseInfo.h"
 
 @implementation BaseInfo
+-(BaseInfo *)getClass{
+    BaseInfo *v = [BaseInfo new];
+    NSDictionary *dic = [NSDictionary dictionaryWithContentsOfFile:[NSSearchPathForDirectoriesInDomains(NSDemoApplicationDirectory, NSUserDomainMask, YES).firstObject stringByAppendingString:@"/baseInfo.plist"]];
+//    此处在想应该整么设计比较合理
+    return v;
+    
+    
+}
 
-
+-(NSString *)uesrName
+{
+    return @"";
+}
 
 
 
